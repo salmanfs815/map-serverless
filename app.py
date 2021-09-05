c9f14d718171a378a0f4479d3000faa801f5adcf
Salman Siddiqui <salmanf.siddiqui@gmail.com>
2021-09-05 01:03:17 -0700

    Send emails for anthology submission and contact forms

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect
 from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
-import datetime, re
 
 app = Flask(__name__)
 
@@ -27,22 +26,30 @@ def submission():
   submission_type = request.form["type"]
   submission_title = request.form["title"]
 
-  timestamp = re.sub("[:.]","-",datetime.datetime.now().isoformat())
-  name_no_spaces = re.sub(r"\s+", "-", full_name.lower())
-  name_alphanum = re.sub(r"[^a-zA-Z0-9-]+", "", name_no_spaces)
+  msg = Message(subject="[MAP] Anthology Submission",
+                reply_to=(full_name, email_address),
+                recipients=["admin@bc-muslim-anthology-proj.ca"])
 
   submission_files = []
   for file in request.files.getlist("submission"):
-    filename = f"{name_alphanum}_{timestamp}_{secure_filename(file.filename)}"
-    file.save(f"uploads/{filename}")
+    filename = secure_filename(file.filename)
+    filepath = f"uploads/{filename}"
+    file.save(filepath)
+    with app.open_resource(filepath) as f:
+      msg.attach(filename, file.mimetype, f.read())
     submission_files.append(filename)
+
+  message = f"New Submission:\n\n\
+Name: {full_name}\n\
+Email: {email_address}\n\
+Type: {submission_type}\n\
+Title: {submission_title}\n\
+Files: {submission_files}"
+
+  msg.body = message
+  mail.send(msg)
   
-  print(f"New Submission:\n\t\
-    Name: {full_name}\n\t\
-    Email: {email_address}\n\t\
-    Type: {submission_type}\n\t\
-    Title: {submission_title}\n\t\
-    Files: {submission_files}")
+  print(message)
   
   return redirect("https://bc-muslim-anthology-proj.netlify.app/submit-entry?success")
 
@@ -50,17 +57,20 @@ def submission():
 def contact():
   name = request.form["name"]
   email = request.form["email"]
-  message = f'Sent by: {name}<{email}>\n\nMessage:\n{request.form["message"]}'
+  message = request.form["message"]
 
   msg = Message(subject="[MAP] Contact Form Submission",
                 reply_to=(name, email),
-                recipients=["admin@bc-muslim-anthology-proj.ca"],
-                body=message)
+                recipients=["admin@bc-muslim-anthology-proj.ca"])
+  
+  messageBody = f"Contact Form:\n\n\
+Name: {name}\n\
+Email: {email}\n\
+message: {message}"
+  
+  msg.body = messageBody
   mail.send(msg)
 
-  print(f"Contact Form:\n\t\
-    Name: {name}\n\t\
-    Email: {email}\n\t\
-    message: {message}")
+  print(messageBody)
   
   return redirect("https://bc-muslim-anthology-proj.netlify.app/contact-us?success")
