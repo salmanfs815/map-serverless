bc7492e00fecfe7a3eca628c1b14835d608c1f47
Salman Siddiqui <salmanf.siddiqui@gmail.com>
2021-09-07 13:01:52 -0700

    Send emails via AWS SES

--- application.py
+++ application.py
@@ -1,19 +1,15 @@
 from flask import Flask, request, redirect
-from flask_mail import Mail, Message
 from werkzeug.utils import secure_filename
 
+import boto3
+from botocore.exceptions import ClientError
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 application = Flask(__name__)
-
-# config values for testing
-application.config["MAIL_SERVER"]= "smtp.mailtrap.io"
-application.config["MAIL_PORT"] = 2525
-application.config["MAIL_USERNAME"] = "5e98fcdb61f449"
-application.config["MAIL_PASSWORD"] = "494caa4a69663f"
-application.config["MAIL_USE_TLS"] = True
-application.config["MAIL_USE_SSL"] = False
-application.config["MAIL_DEFAULT_SENDER"] = "contact@bc-muslim-anthology-proj.netlify.app"
-
-mail = Mail(application)
 
 @application.route("/")
 def hello_world():
@@ -26,17 +22,21 @@ def submission():
   submission_type = request.form["type"]
   submission_title = request.form["title"]
 
-  msg = Message(subject="[MAP] Anthology Submission",
-                reply_to=(full_name, email_address),
-                recipients=["admin@bc-muslim-anthology-proj.ca"])
+  msg = MIMEMultipart()
+  msg["Subject"] = "[MAP] Anthology Submission"
+  msg["From"] = "bc-muslim-anthology-proj@protonmail.com"
+  msg["To"] = "salmanf.siddiqui@gmail.com"
 
   submission_files = []
   for file in request.files.getlist("submission"):
     filename = secure_filename(file.filename)
-    filepath = f"uploads/{filename}"
-    file.save(filepath)
-    with application.open_resource(filepath) as f:
-      msg.attach(filename, file.mimetype, f.read())
+    
+    part = MIMEApplication(file.read())
+    part.add_header("Content-Disposition",
+                    "attachment",
+                    filename=filename)
+    msg.attach(part)
+
     submission_files.append(filename)
 
   message = f"New Submission:\n\n\
@@ -46,11 +46,27 @@ Type: {submission_type}\n\
 Title: {submission_title}\n\
 Files: {submission_files}"
 
-  msg.body = message
-  mail.send(msg)
-  
+  body = MIMEText(message, "plain")
+  msg.attach(body)
+
+  try:
+    ses_client = boto3.client("ses", region_name="us-west-2")
+    response = ses_client.send_raw_email(
+        Source="bc-muslim-anthology-proj@protonmail.com",
+        Destinations=["salmanf.siddiqui@gmail.com"],
+        RawMessage={"Data": msg.as_string()}
+    )
+    print(response)
+  except ClientError as err:
+    if "InvalidParameterValue" in str(err):
+      print("ERROR sending email: message length exceeds allowed maximum (10MB)")
+      return redirect("https://bc-muslim-anthology-proj.netlify.app/submit-entry?error=maxsize")
+    else:
+      print("ERROR sending email:")
+      print(err)
+      return redirect("https://bc-muslim-anthology-proj.netlify.app/submit-entry?error")
+
   print(message)
-  
   return redirect("https://bc-muslim-anthology-proj.netlify.app/submit-entry?success")
 
 @application.route("/contact", methods=["POST"])
@@ -59,20 +75,37 @@ def contact():
   email = request.form["email"]
   message = request.form["message"]
 
-  msg = Message(subject="[MAP] Contact Form Submission",
-                reply_to=(name, email),
-                recipients=["admin@bc-muslim-anthology-proj.ca"])
-  
-  messageBody = f"Contact Form:\n\n\
+  msg = MIMEMultipart()
+  msg["Subject"] = "[MAP] Contact Form Submission"
+  msg["From"] = "bc-muslim-anthology-proj@protonmail.com"
+  msg["To"] = "salmanf.siddiqui@gmail.com"
+
+  message = f"Contact Form:\n\n\
 Name: {name}\n\
 Email: {email}\n\
 message: {message}"
-  
-  msg.body = messageBody
-  mail.send(msg)
 
-  print(messageBody)
-  
+  body = MIMEText(message, "plain")
+  msg.attach(body)
+
+  try:
+    ses_client = boto3.client("ses", region_name="us-west-2")
+    response = ses_client.send_raw_email(
+        Source="bc-muslim-anthology-proj@protonmail.com",
+        Destinations=["salmanf.siddiqui@gmail.com"],
+        RawMessage={"Data": msg.as_string()}
+    )
+    print(response)
+  except ClientError as err:
+    if "InvalidParameterValue" in str(err):
+      print("ERROR sending email: message length exceeds allowed maximum (10MB)")
+      return redirect("https://bc-muslim-anthology-proj.netlify.app/submit-entry?error=maxsize")
+    else:
+      print("ERROR sending email:")
+      print(err)
+      return redirect("https://bc-muslim-anthology-proj.netlify.app/submit-entry?error")
+
+  print(message)
   return redirect("https://bc-muslim-anthology-proj.netlify.app/contact-us?success")
 
 
